f350e62a73eb061ada4cd2741d8265064e0246b6
vatojavier <vatojavier@gmail.com>
2020-02-16 11:34:05 +0100

    joinhtml++

--- static/js/join.js
+++ static/js/join.js
@@ -2,15 +2,16 @@ function joinFunction(id){
     
     /*alert("concha" + id);*/
     var url_join = "/join_game/" + id;
-    var boton = "#btn_entrar_" + id;
-    var texto_suc = "#join_succes_" + id;
 
     $.ajax({url: url_join,
             dataType: "text",
 
          complete: function(result){
-            $(boton).show();
-            $(texto_suc).show();
+
+            $("#boton_entrar_"+id).hide();
+            $("#join_succes_" + id).show();
+            $("#btn_jugar_"+id).show();
+
             
         },
     });
